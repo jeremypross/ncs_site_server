28fc25d1797db1073de12629ad5d2065301ede29
Jeremy Ross <jeremypross@gmail.com>
2025-05-05 17:13:25 -0700

    added /signin route functionality

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -20,5 +20,26 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    // error check
+    if (!email || !password) {
+        return res.status(402).send({ error: 'Must provide email and password' });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send({ error: 'Email not found' });
+    }
+    try {
+        await user.comparePassword(password);
+        // generate JWT and sent it to user so they can use it to authenticate on future requests
+        const token = jwt.sign({ userId: user._id}, 'MY_SECRET_KEY');
+        // send token
+        res.send({ token })
+    } catch (err) {
+        return res.status(422).send({ error: 'invalid email or password' });
+    }
+});
+
 module.exports = router;
 
