21bb567659917e801c48f2328ae4a6129d44cfff
Jeremy Ross <jeremypross@gmail.com>
2025-05-12 14:40:56 -0700

    added Post.js model

---
+++ src/models/Post.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require('mongoose');
+
+const metaTags = new mongoose.Schema({
+    name: String
+});
+
+const postSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Tupes.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        default: ''
+    },
+    body: {
+        type: String,
+        default: ''
+    },
+    imageUrl: {
+        type: String,
+        default: ''
+    },
+    tags: {
+        type: [metaTags],
+        default: undefined
+    }
+});
+
+mongoose.model('Post', postSchema);
